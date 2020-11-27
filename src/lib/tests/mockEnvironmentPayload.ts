179c2e9f539541ba1380cbe6f8c8ceba8b4fe03d
Mounir Dhahri <soussouheros@gmail.com>
2020-11-27 15:07:20 +0100

    added artist about shows tests

--- src/lib/tests/mockEnvironmentPayload.ts
+++ src/lib/tests/mockEnvironmentPayload.ts
@@ -1,7 +1,10 @@
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
+import { MockResolvers } from "relay-test-utils/lib/RelayMockPayloadGenerator"
 
 export type RelayMockEnvironment = ReturnType<typeof createMockEnvironment>
 
-export const mockEnvironmentPayload = (mockEnvironment: RelayMockEnvironment, passedProps = {}) => {
-  mockEnvironment.mock.resolveMostRecentOperation((operation) => MockPayloadGenerator.generate(operation, passedProps))
+export const mockEnvironmentPayload = (mockEnvironment: RelayMockEnvironment, mockResolvers?: MockResolvers) => {
+  mockEnvironment.mock.resolveMostRecentOperation((operation) =>
+    MockPayloadGenerator.generate(operation, mockResolvers)
+  )
 }
